0b7b9d7f3725157d071860c80c2bd0573b2e85ec
Yevhen-H <y.hurtovyi@yahoo.com>
2019-03-25 21:48:16 -0500

    Imgur Test create functions with refactoring in ALL file

--- search.py
+++ search.py
@@ -1,13 +1,14 @@
 from selenium import webdriver
-driver = webdriver.Chrome()  # replace on Firefox if needed
+driver = webdriver.Chrome() #Firefox: executable_path="C:/Users/yhurt/PycharmProjects/IMGUR_TEST/venv37/geckodriver.exe"
 import time
+from selenium.webdriver.common.action_chains import ActionChains
 
 
-
-driver.implicitly_wait(5)
+driver.implicitly_wait(7)
+driver.maximize_window()
 driver.get("https://imgur.com/")
+#sign in
 driver.find_element_by_css_selector("a.Navbar-signin").click()
-#login
 driver.find_element_by_id("username").send_keys("SomeNewUser")
 driver.find_element_by_id("password").send_keys("secret1")
 driver.find_element_by_name("submit").click()
@@ -16,10 +17,12 @@ driver.find_element_by_css_selector("input.Searchbar-textInput").send_keys("spac
 driver.find_element_by_css_selector("button.Searchbar-submitInput").click()
 #click by picture
 driver.find_element_by_id("QSymA").click()
+#click next
 driver.find_element_by_css_selector("div.btn.btn-action.navNext").click()
-#write comment
-driver.find_element_by_css_selector("spin.textarea.commentForm").send_keys("It is Awesome!")
-#driver.find_element_by_css_selector("button.post-action-icon post-action-favorite icon-favorite-outline").click()
-#
-#driver.find_element_by_id("submit-comment").click()
-time.sleep(8)
+time.sleep(5)
+#sign out
+dropdown = driver.find_element_by_css_selector("a.topbar-icon.account-user-name")
+hover = ActionChains(driver).move_to_element(dropdown)
+hover.perform()
+driver.find_element_by_css_selector("button.log-out").click()
+driver.quit()
--- sign_in.py
+++ sign_in.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-driver = webdriver.Chrome()  # replace on Firefox if needed
+driver = webdriver.Chrome()#Firefox: executable_path="C:/Users/yhurt/PycharmProjects/IMGUR_TEST/venv37/geckodriver.exe"
 import time
 import os
 
@@ -13,12 +13,11 @@ driver.find_element_by_id("password").send_keys("secret1")
 driver.find_element_by_name("submit").click()
 #create a new post
 driver.find_element_by_css_selector("span.Button-label").click()
-fileinput = driver.find_element_by_css_selector("label.browse-btn").click()
+driver.find_element_by_css_selector("label.browse-btn").click()
 driver.find_element_by_css_selector("label.browse-btn").send_keys("C:/Users/yhurt/image1.png")
 
 #elm = driver.find_element_by_css_selector("label.browse-btn")
 #elm.send_keys(os.getcwd() + "C:/Users/yhurt/image1.png")
-
 
 
 #fileInput  = driver.find_element_by_css_selector("label.browse-btn")
@@ -34,7 +33,3 @@ time.sleep(6)
 
 #driver.find_element_by_css_selector("div.Dropdown-title").click()
 #driver.find_element_by_css_selector("img.sign-out").click()
-
-
-
-time.sleep(2)
